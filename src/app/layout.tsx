a10a702e9132fe485467e785c784029222a31987
Noah Chomsky <nb.chomsky@gmail.com>
2025-03-28 12:47:53 -0400

    Suppress Hydration Warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${openSans.className} antialiased`}>
-      <body>
+      <body suppressHydrationWarning>
         {children}
       </body>
     </html>
