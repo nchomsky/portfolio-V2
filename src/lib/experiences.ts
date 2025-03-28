964c4668f2c3e388c98b713124e300b8f682f2b3
Noah Chomsky <nb.chomsky@gmail.com>
2025-03-28 12:45:29 -0400

    Make Experience Summaries more concise

--- src/lib/experiences.ts
+++ src/lib/experiences.ts
@@ -5,10 +5,7 @@ const apple: Experience = {
     position: 'Software Developer',
     startDate: 'April 2023',
     endDate: 'Present',
-    summary: `I worked with two other developers to refactor an existing project built with GatsbyJS, transitioning it to Next.js to efficiently serve documents from various GitHub repositories.
-                This refactor significantly improved the application's build time by over 90% and included the implementation of a proprietary analytics tool to track usage. 
-                Additionally, I collaborated with a team of around 15 developers to build an internal tool used by over 700 SREs weekly to resolve issues, view reports, and gain insights into service health and reliability. 
-                As part of this effort, I led a major refactor of the app's permission management system. To complete my work on this refactor, I created and modified APIs using Java and Spring to interact with various services.`,
+    summary: `Collaborated with two developers to build an application using Next.js to efficiently serve documents from various GitHub repositories. Additionally, I worked with a team to build a tool used by SREs to resolve issues, view reports, and gain insights into service health and reliability. As part of this effort, I led a major refactor of the app's permission management system.`,
     skills: [
         'React', 
         'Redux', 
@@ -31,9 +28,7 @@ const pgDev: Experience = {
     position: 'Software Developer',
     startDate: 'July 2022',
     endDate: 'April 2023',
-    summary: `I worked with a team of 14 developers to build an enterprise application consisting of four separate applications that integrate with SAP and Primavera P6. 
-                In each release cycle, I complete approximately 18% of the total tickets. I implemented Ag-Grid React to efficiently display and organize data synced from the Primavera API and SAP. 
-                Additionally, I led all development efforts on DevExpress Reporting, which involved creating and modifying reports as well as managing the database schema that these reports rely on.`,
+    summary: `Engaged with 14 developers to build an enterprise application that integrates with SAP and Primavera P6. Implemented Ag-Grid React to efficiently display and organize data synced from the Primavera API and SAP. Additionally, led all development efforts on DevExpress Reporting, which involved creating and modifying reports as well as managing the database schema that these reports rely on.`,
     skills: [
         'React', 
         'Redux',
@@ -54,10 +49,7 @@ const pgImp: Experience = {
     position: 'Implementation Specialist',
     startDate: 'April 2021',
     endDate: 'June 2022',
-    summary: `I worked with SQL scripts to import, export, and modify data in customer databases, ensuring smooth data management. 
-            I also created and updated DevExpress reports that dynamically adjusted based on customer input within their workflows. 
-            Additionally, I coordinated with customers and the development team to plan software releases and implement customer-specific configurations. 
-            To support collaboration, I actively managed the Git repository and guided team members on using Git to track changes for the reports we built.`,
+    summary: `Managed SQL scripts for data operations in customer databases. Developed dynamic DevExpress reports, coordinated software releases, and implemented customer-specific configurations. Maintained the Git repository and guided team members on version control.`,
     skills: [
         'HTML',
         'CSS',
@@ -74,7 +66,7 @@ const infosys: Experience = {
     position: 'Implementation Specialist',
     startDate: 'June 2019',
     endDate: 'June 2020',
-    summary: `I worked as part of a team to develop a Credit Card Application that enabled registered users to view, update, add, and delete payment methods. The client-side interface was built using Angular 7, while CRUD operations were managed through the JPA EntityManager interface in the Persistence Layer. I also had the opportunity to present our work to the CTO of a major company, where my teammates and I provided an in-depth overview of the technologies we were using. Additionally, I actively sought opportunities for professional growth, such as completing a two-week course to become internally certified in the Agile methodology.`,
+    summary: `Developed a Credit Card Application with Angular 7 and JPA for payment management. Presented the project to a company's CTO and earned internal Agile certification`,
     skills: [
         'React', 
         'Redux',
