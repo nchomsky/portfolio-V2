45a8c1cb45df863772e9f1dfbe6b13330c19860c
Noah Chomsky <nb.chomsky@gmail.com>
2025-03-28 23:46:52 -0400

    Better Styling for Experience Card

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -4,12 +4,12 @@ import Tag from './Tag';
 const ExperienceCard = ({ experience } : { experience : Experience }) => {
   
     return (
-            <div className="group md:flex lg:p-8 my-4 mx-8 rounded-lg lg:hover:shadow-lg lg:hover:bg-comp-bg">
-                <p className="text-base font-light lg:w-1/4">{experience.startDate} - {experience.endDate}</p>
+            <div className="group md:flex lg:p-8 rounded-lg lg:hover:shadow-lg lg:hover:bg-comp-bg">
+                <p className="text-xs text-muted font-semibold lg:w-1/4">{experience.startDate} - {experience.endDate}</p>
                 <div className="lg:w-3/4">
-                    <h3 className=" mb-4 font-bold antialiased lg:group-hover:text-accent">{experience.company} - {experience.position}</h3>
-                    <p className="mb-4 antialiased">{experience.summary}</p>
-                    <ul className="flex flex-wrap">
+                    <h3 className=" font-bold antialiased lg:group-hover:text-accent">{experience.company} - {experience.position}</h3>
+                    <p className="mt-2 text-secondary text-sm/6">{experience.summary}</p>
+                    <ul className="mt-2 flex flex-wrap">
                         {experience.skills.map(skill => (
                         <Tag key={skill} text={skill}/>
                         ))}
