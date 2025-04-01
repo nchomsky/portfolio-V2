a2a924c7987e4574f1eaedfc6056951b906ad548
Noah Chomsky <nb.chomsky@gmail.com>
2025-03-31 22:05:14 -0400

    Add Social Icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import ExperienceCard from "@/components/ExperienceCard";
 import Particles from "@/components/Particles";
 import { experiences } from "@/lib/experiences";
+import GitHubIcon from "@/public/icons/githubIcon";
+import LinkedInIcon from "@/public/icons/linkedInIcon";
+import ResumeIcon from "@/public/icons/resumeIcon";
 
-const testText = `I’m a developer passionate about creating software that is both functional and beautiful. I have experience working with a variety of technologies and languages, including JavaScript, TypeScript, React, Node.js, and Python. I have a strong foundation in computer science and software engineering principles, and I’m always looking to learn new things and improve my skills.
+const testText = `I’m a developer passionate about creating software that is 
+            both functional and beautiful. I have experience working with a variety 
+            of technologies and languages, including JavaScript, TypeScript, React, 
+            Node.js, and Python. I have a strong foundation in computer science and 
+            software engineering principles, and I’m always looking to learn new things and improve my skills.
             Currently, I'm a Software Engineer Contractor at Apple. 
-            \nI have experience working both for large enterprises and smaller startups, and I’m comfortable working in fast-paced environments and collaborating with cross-functional teams. I’m a strong communicator and problem solver, and I’m always looking for ways to improve my work and the work of those around me. I’m excited about the opportunity to work on new and challenging projects, and I’m always looking for ways to grow and develop as a developer.
+            I have experience working both for large enterprises and smaller startups, 
+            and I’m comfortable working in fast-paced environments and collaborating with 
+            cross-functional teams. I’m a strong communicator and problem solver, and I’m always 
+            looking for ways to improve my work and the work of those around me. I’m excited about the 
+            opportunity to work on new and challenging projects, and I’m always looking for ways to grow
+             and develop as a developer.
             `;
 
 export default function Home() {
@@ -14,16 +26,33 @@ export default function Home() {
       {/* Left Side */}
       <div className="lg:h-screen lg:sticky lg:top-0 lg:w-[45%]">
         <div className="lg:my-20">
-          <h1 className="text-5xl font-bold lg:text-6xl">Noah Chomsky</h1>
-          <h2 className="text-2xl lg:text-3xl">Software Developer</h2>
+          <h1 className="text-4xl mb-2 font-bold lg:text-5xl">Noah Chomsky</h1>
+          <h2 className="mb-4 text-2xl text-subtle lg:text-3xl ">Software Developer</h2>
           {/* Add LinkedIn, GitHub, and Resume Icons */}
+          <ul aria-label="Social Media List" className="flex mb-20">
+            <li className="mr-4 shrink-0">
+              <a href="https://www.linkedin.com/in/nchomsky/" target="_blank">
+                <LinkedInIcon className="fill-subtle hover:fill-secondary hover:cursor-pointer transition duration-300 ease-in-out hover:-translate-y-1"/>
+              </a>
+            </li>
+            <li className="mr-4 shrink-0">
+              <a href="https://github.com/nchomsky" target="_blank">
+                <GitHubIcon className="fill-subtle hover:fill-secondary hover:cursor-pointer transition duration-300 ease-in-out hover:-translate-y-1"/>
+              </a>
+            </li>
+            <li className="mr-4 shrink-0">
+              <a href="" target="_blank">
+                <ResumeIcon className="fill-subtle hover:fill-secondary hover:cursor-pointer transition duration-300 ease-in-out hover:-translate-y-1"/>
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
       {/* Right Side */}
       <div className="lg:min-h-screen lg:overflow-y-auto lg:w-[55%]">
         <section id="about" className="lg:mr-4">
         <h2 className="ml-8 text-lg lg:hidden">About</h2>
-          <div className="mt-10 lg:mt-20 my-4 mx-8 text-secondary">
+          <div className="mt-10 lg:mt-20 my-4 mx-8 text-subtle">
             <p>{testText}</p>
           </div>
         </section>
--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,11 +1,12 @@
 import Experience from '@/lib/types'; 
 import Tag from './Tag';
 
+// Maybe turn this into a generic card component once I have projects included as well...
 const ExperienceCard = ({ experience } : { experience : Experience }) => {
   
     return (
             <div className="group mt-2 md:flex lg:p-8 rounded-lg lg:hover:inset-shadow-md lg:hover:bg-comp-bg/50">
-                <p className="text-xs text-muted font-semibold lg:w-1/4">{experience.startDate} - {experience.endDate}</p>
+                <p className="text-xs text-subtle font-semibold lg:w-1/4">{experience.startDate} - {experience.endDate}</p>
                 <div className="lg:w-3/4">
                     <h3 className=" font-bold antialiased lg:group-hover:text-accent">{experience.company} - {experience.position}</h3>
                     <p className="mt-2 text-secondary text-sm/6">{experience.summary}</p>
